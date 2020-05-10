9e552a7a1a4adf0dd9c2e86415f84b79899de94d
Osman Setuval <osman@ntsaobraz.com.br>
2020-05-10 10:14:59 -0300

    Desafio de fundamentos de nodes

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (acumulador: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            acumulador.income += transaction.value;
+            break;
+          case 'outcome':
+            acumulador.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return acumulador;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
